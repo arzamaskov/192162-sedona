e9b83f1a5d94f6ed57d2506e396f6c902860e989
arzamaskov <arzamaskov@gmail.com>
2016-09-04 23:28:37 +0400

    Добавил скрипт

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var link = document.querySelector(".search-form-btn");
+var searchForm = document.querySelector(".search-form");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  if (searchForm.classList.contains("search-form-visible")) {
+    searchForm.classList.add("search-form-invisible");
+    searchForm.classList.remove("search-form-visible");
+  } else {
+    searchForm.classList.add("search-form-visible");
+    searchForm.classList.remove("search-form-invisible");
+  };
+});
